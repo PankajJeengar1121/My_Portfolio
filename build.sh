7c5243fc471c956e594846c0505ee9a2b7d07fa4
PankajJeengar1121 <143752015+PankajJeengar1121@users.noreply.github.com>
2026-02-16 01:59:18 +0530

    Revise build.sh for python3.12 and add echo statements
    Updated build script to use python3.12 and added build messages.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+# root/build.sh
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
+echo "Build finished!"
